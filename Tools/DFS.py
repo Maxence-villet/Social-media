bbb7c76fd85fcddf9e14cc48b531dfcb48bd0d2b
Amandine GUISY <aguisy@yahoo.fr>
2025-01-07 15:08:06 +0100

    Feature/DFS Addition of the doctring and commentary for the DFS file

--- Tools/DFS.py
+++ Tools/DFS.py
@@ -3,14 +3,39 @@ class Dfs:
         self.visited_nodes = []
 
     def visited_dfs(self, graph_tuple, start):
+        """
+        Initialize the visited nodes and do the dfs function below
+
+        Args:
+        ------
+        [graph_tuple] (tuple of a list and of a list of a list) : a tuple where we can find our matrix and our nodes
+        [start] (str, int, ... whatever you want) : this node is where we want to applie the DFS algorithm from
+
+        Returns:
+        ---------
+        type : (list) of visited nodes with the DFS applies
+        """
         SOM, MAT = graph_tuple 
         self.visited_nodes = []
-        self.node_to_index = {node: i for i, node in enumerate(SOM)}
-        self.dfs(graph_tuple, start)
+        self.node_to_index = {node: i for i, node in enumerate(SOM)} # create a ditionnary where each nodes have their unique index value
+        self.dfs(graph_tuple, start) # applies the DFS algorithm
     
         return self.visited_nodes
 
     def dfs(self, graph_tuple, node) :
+        """
+        A recursion function where we applie the DFS algorithm
+
+        Args:
+        ------
+        [graph_tuple] (tuple of a list and of a list of a list) : a tuple where we can find our matrix and our nodes
+        [node] (str, int, ... whatever you want) : this node is where we want to applie the DFS algorithm from,
+        and is from a seed at the beggining of this recursion
+
+        Returns:
+        ---------
+        type : (fonction) This function returns the same function to apply recursion, which will allow obtaining all possible depth paths."
+        """
         nodes, matrix = graph_tuple
 
         if node not in self.visited_nodes :
