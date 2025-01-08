99a21b1895de4f8e9dbeea85fd545f3242801b13
Amandine GUISY <aguisy@yahoo.fr>
2025-01-08 13:26:47 +0100

    Feature/TopInfluencer Addition of the doctring for the Top_Influencer file

--- Tools/Top_Influencer.py
+++ Tools/Top_Influencer.py
@@ -4,6 +4,17 @@ class Top1:
         self.node_to_index = {}
 
     def influencer(self, graph_tuple):
+        """
+        This fonction enumerate the neighbor(e) of each nodes and save it in a list
+
+        Args:
+        ------
+        [graph_tuple] (tuple of a list and of a list of a list) : a tuple where we can find our matrix and our nodes
+
+        Returns:
+        ---------
+        type : (list) of the node(s) with the most amount of neighbor
+        """
         SOM, MAT = graph_tuple
         self.top = []
         list_neighbor = [0] * len(SOM)
@@ -18,6 +29,20 @@ class Top1:
         return [SOM[i] for i in self.top]
 
     def Top_Infl(self, list_neighbor):
+        """
+        This function takes the biggest neighbor of the list and compared to all of the list of neighbor.
+        If this is equal to the biggest neighbor possible it's save on a list. In case two or more nodes
+        have the same amount of neighbor(s)
+
+        Args:
+        ------
+        [list_neighbor] (list) : a list of the amount of neighbor(s) foreach nodes. Where the index of the list
+        corresponds with the index of the SUM list (the list of the nodes in the tuple graph_tuple)
+
+        Returns:
+        ---------
+        type : (list) of the index of the node(s) with the most amount of neighbor(s)
+        """
         if not list_neighbor:
             print("La liste ne peut pas être vide")
             return None
