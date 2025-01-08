55f7f5c97b8387c5a2ba0a429e402a8f2a9ed896
Amandine GUISY <aguisy@yahoo.fr>
2025-01-08 13:41:06 +0100

    Feature/TopInfluencer addition of the commentary in the Top_influencer.py file

--- Tools/Top_Influencer.py
+++ Tools/Top_Influencer.py
@@ -17,12 +17,12 @@ class Top1:
         """
         SOM, MAT = graph_tuple
         self.top = []
-        list_neighbor = [0] * len(SOM)
-        self.node_to_index = {node: i for i, node in enumerate(SOM)}
+        list_neighbor = [0] * len(SOM) # It will be increment so we need to set them up at 0
+        self.node_to_index = {node: i for i, node in enumerate(SOM)} # It's a dictionnary where each nodes have their index as key
 
         for index in range(len(SOM)):
-            for i, is_connected in enumerate(MAT):
-                if is_connected[index] == 1:
+            for i, is_connected in enumerate(MAT): # it will travel from column to column 
+                if is_connected[index] == 1: # if it's a neighbor in the matrix (=1) it will add a new neighbor in the list at the index of the node who corresponds
                     list_neighbor[index] += 1
 
         self.top = self.Top_Infl(list_neighbor)
@@ -43,12 +43,12 @@ class Top1:
         ---------
         type : (list) of the index of the node(s) with the most amount of neighbor(s)
         """
-        if not list_neighbor:
+        if not list_neighbor: # a protection if the list is empty
             print("La liste ne peut pas être vide")
             return None
 
-        top_value = max(list_neighbor)
-        top_list = [index for index, value in enumerate(list_neighbor) if value == top_value]
+        top_value = max(list_neighbor) # get the biggest value of neighbor
+        top_list = [index for index, value in enumerate(list_neighbor) if value == top_value] # if the value is the same as the top value found, it will register the index in the list, in order to find the node associated with the index
     
         return top_list
 
