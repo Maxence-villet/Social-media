00ee271f56a0896234593ba5ffe076fb71027c78
Amandine GUISY <aguisy@yahoo.fr>
2025-01-09 15:55:20 +0100

    Feature/PropagationPath fixing the propagation module, it's now working perfectly

--- Tools/propagation_path.py
+++ Tools/propagation_path.py
@@ -1,28 +1,32 @@
-from Tools import BFS
-
 class PropagationPath:
 
     def __init__(self):
-        self.visited = []
         self.node_to_index = {}
 
     def best_path(self, graph_tuple, start, end):
-        bfs_start = BFS.BFS.bfs(graph_tuple, start)
+        
         SOM, MAT = graph_tuple
-        self.visited = []
         self.node_to_index = {node: i for i, node in enumerate(SOM)}
 
-        while bfs_start:
-            current, path = bfs_start.popleft()
+        if start not in SOM or end not in SOM:
+            raise ValueError(f"Node {start} or node {end} is not in the graph.")
+
+        queue = [(start, [start])]
+
+        while queue:
+            current, path = queue.pop(0)
+
             if current == end:
                 return path
-            if current not in self.visited:
-                self.visited.append(current)
-                current_index = self.node_to_index[current]
-                for neighbor_index, is_connected in enumerate(MAT[current_index]):
-                    if is_connected:
-                        neighbor = SOM[neighbor_index]
-                        bfs_start.append((neighbor, path + [neighbor]))
+            
+            current_index = self.node_to_index[current]
 
-        raise ValueError(f"No path find between {start} eand {end}.")
+
+            for neighbor_index, is_connected in enumerate(MAT[current_index]):
+                neighbor = SOM[neighbor_index]
+
+                if is_connected == 1 and neighbor not in path:
+                    queue.append((neighbor, path + [neighbor]))
+
+        raise ValueError(f"No path find between {start} and {end}.")
 
