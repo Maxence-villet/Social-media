b497be1fcffa791fed45c556b9f3ed61e63eff29
Maxence.villet <maxence.villetschoumaker@gmail.com>
2025-01-11 09:15:48 +0100

    Feature/add generate communities function

--- Tools/generate_graph.py
+++ Tools/generate_graph.py
@@ -3,27 +3,19 @@ import random
 
 class Generate_Graph:
     def __init__(self):
-        pass
+        self.vertices
+        self.communities = []
 
     def generate_graph(self, oriented, num_vertices, min_degree, max_degree, num_communities, max_distance):
-        vertices = self.generate_vertices(num_vertices)
+        self.generate_vertices(num_vertices)
+        self.generate_communities(num_communities)
         
-        # Générer les communautés
-        communities = []
-        remaining_vertices = vertices.copy()
-        for _ in range(num_communities):
-            if not remaining_vertices:
-                break
-            community_size = random.randint(1, len(remaining_vertices))
-            community = random.sample(remaining_vertices, community_size)
-            communities.append(community)
-            remaining_vertices = [v for v in remaining_vertices if v not in community]
         
         # Générer les arcs/arêtes
         edges = []
-        for vertex in vertices:
+        for vertex in self.vertices:
             degree = random.randint(min_degree, max_degree)
-            possible_neighbors = [v for v in vertices if v != vertex]
+            possible_neighbors = [v for v in self.vertices if v != vertex]
             neighbors = random.sample(possible_neighbors, min(degree, len(possible_neighbors)))
             
             for neighbor in neighbors:
@@ -43,7 +35,7 @@ class Generate_Graph:
             
         return vertices, edges, communities
 
-    def generate_vertices(self, num_vertices):
+    def generate_vertices(self, num_vertices) -> list[int]:
         """
         this function generate an vertices list
 
@@ -62,7 +54,36 @@ class Generate_Graph:
         if type(num_vertices) != int:
             raise ValueError("Le nombre de sommets doit être un entier")
         
-        return list(range(num_vertices))
+        self.vertices = list(range(num_vertices))
+    
+    def generate_communities(self, num_communities) -> list[int]:
+        """
+        this function generate an communities list
+
+        Args:
+        -----
+        num_communities (int) : number of communities
+
+        Returns:
+        --------
+        (list[int]) : list of communities
+
+        """
+        if not num_communities:
+            raise ValueError("num_communities ne peux pas être vide")
+        
+        if type(num_communities) != int:
+            raise ValueError("Le nombre de communautées doit être un entier")
+        
+        remaining_vertices = self.vertices.copy()
+        for _ in range(num_communities):
+            if not remaining_vertices:
+                break
+            community_size = random.randint(1, len(remaining_vertices))
+            community = random.sample(remaining_vertices, community_size)
+            self.communities.append(community)
+            remaining_vertices = [v for v in remaining_vertices if v not in community]
+
 
     def save_graph_to_file(self, filename, oriented, vertices, edges):
         with open(filename, 'w') as file:
